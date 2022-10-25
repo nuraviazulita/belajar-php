6ba8664a30416858a8b8d16126d0179a44c8257e
Nur Avia Zulita <aviazulita@gmail.com>
2022-10-25 20:35:57 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Nur Avia Zulita";
+$umur = 21;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaAdik = "Muhammad Lukman Haqiki";
+$umurAdik = 12;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+$selisihUmur = $umur-$umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun."
+
+
+?>
